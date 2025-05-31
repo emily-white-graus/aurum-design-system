4f1ba7445cf05dea6d079b1521a68a884a80049c
emily-white-graus <emilywhitegraus@gmail.com>
2025-05-31 18:45:15 +0200

    figma automation try1

---
+++ generate-css.js
@@ -0,0 +1,38 @@
+// generate-css.js
+import tokens from './assets/design-tokens.tokens.json' with { type: 'json' };
+import fs from 'node:fs';
+
+const { primitives, "design-tokens": rawDesignTokens } = tokens;
+
+function parseTokens(tokensObject) {
+  const result = {};
+  const traverseTokensTree = (node, path = []) => {
+    for (const key in node) {
+      if ('value' in node[key]) {
+        const fullTokenKey = [...path, key].join('-');
+        result[fullTokenKey] = node[key].value;
+      } else {
+        traverseTokensTree(node[key], [...path, key]);
+      }
+    }
+  };
+  traverseTokensTree(tokensObject);
+  return result;
+}
+
+const primitivesMap = parseTokens(primitives);
+
+const designTokens = Object.entries(parseTokens(rawDesignTokens)).reduce((acc, [key, value]) => {
+  return {
+    ...acc,
+    [key]: primitivesMap[value.replace('{primitives.', '').replace('}', '').replace('.', '-')]
+  };
+}, {});
+
+const designTokensVariables = Object.entries(designTokens).reduce((acc, [key, value]) => {
+  return `${acc}\n--${key}: ${value};`;
+}, '');
+
+const template = `:root {${designTokensVariables}\n}`;
+
+fs.writeFileSync('assets/design-tokens.css', template, { flag: 'w' });
